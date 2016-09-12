142b1c2330765f28f289cdce0d76a509e8c91449
Dan Newman <dnewman@polar3d.com>
2016-09-12 14:04:40 -0700

    M212 X0 Y0 Z-1 as defaults

--- Marlin11a/Configuration.h
+++ Marlin11a/Configuration.h
@@ -353,7 +353,7 @@ const bool Z_MIN_ENDSTOP_INVERTING = true; // set to true to invert the logic of
   // these are the offsets to the prob relative to the extruder tip (Hotend - Probe)
   #define X_PROBE_OFFSET_FROM_EXTRUDER_DEFAULT 0
   #define Y_PROBE_OFFSET_FROM_EXTRUDER_DEFAULT 0
-  #define Z_PROBE_OFFSET_FROM_EXTRUDER_DEFAULT 0
+  #define Z_PROBE_OFFSET_FROM_EXTRUDER_DEFAULT -1
 
   #define Z_RAISE_BEFORE_HOMING 2       // (in mm) Raise Z before homing (G28) for Probe Clearance.
                                         // Be sure you have this distance over your Z_MAX_POS in case
