f0591a3951829b8038561f772c29a275d9ed4145
Dan Newman <dnewman@polar3d.com>
2016-09-13 08:13:48 -0700

    Bump version to 1.1.160913

--- Marlin11a/Configuration.h
+++ Marlin11a/Configuration.h
@@ -82,7 +82,7 @@
 // startup. Implementation of an idea by Prof Braino to inform user that any changes made to this
 // build by the user have been successfully uploaded into firmware.
 #define STRING_VERSION_CONFIG_H __DATE__ " " __TIME__ // build date and time
-#define STRING_CONFIG_H_AUTHOR "(wsteele, PolarBoard 160912" MOTHERBOARD_VARIANT_STR ")" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(wsteele, PolarBoard 160913" MOTHERBOARD_VARIANT_STR ")" // Who made the changes.
 
 // Define this to set a custom name for your generic Mendel,
 // #define CUSTOM_MENDEL_NAME "This Mendel"
